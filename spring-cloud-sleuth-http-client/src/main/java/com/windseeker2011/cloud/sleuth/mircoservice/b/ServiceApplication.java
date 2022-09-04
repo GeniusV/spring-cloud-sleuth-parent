60479f4b9b124362ed1b989b9374aa548f67aace
GeniusV <eliot310100@gmail.com>
2022-09-04 15:54:01 +0800

    Update

--- spring-cloud-sleuth-http-client/src/main/java/com/windseeker2011/cloud/sleuth/mircoservice/b/ServiceApplication.java
+++ spring-cloud-sleuth-http-client/src/main/java/com/windseeker2011/cloud/sleuth/mircoservice/b/ServiceApplication.java
@@ -13,6 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * 第二个微服务
  *
@@ -23,6 +28,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @SpringBootApplication
 public class ServiceApplication {
+
+    @Autowired
+    private ThreadPoolExecutor threadPoolExecutor;
 
     @Autowired
     private Tracer tracer;
@@ -52,7 +60,13 @@ public class ServiceApplication {
     @GetMapping(value = "/2")
     public String m2() {
         log.info("我是微服务2号。。。");
-        ResponseEntity<String> result = restTemplate().getForEntity("http://127.0.0.1:8883/1", String.class);
+        List<Future<ResponseEntity<String>>> respList = IntStream.range(0, 3).mapToObj((i) -> {
+                    return threadPoolExecutor.submit(() -> {
+                        ResponseEntity<String> result = restTemplate().getForEntity("http://127.0.0.1:8883/1", String.class);
+                        return result;
+                    });
+                })
+                .collect(Collectors.toList());
         // Start a span. If there was a span present in this thread it will become
 // the `newSpan`'s parent.
         Span newSpan = this.tracer.nextSpan().name("calculateTax");
@@ -69,7 +83,12 @@ public class ServiceApplication {
             // the span to send it to a distributed tracing system e.g. Zipkin
             newSpan.end();
         }
-        return result.getBody();
+        return respList.toString();
+    }
+
+    @Bean
+    public ThreadPoolExecutor threadPoolExecutor() {
+        return new ThreadPoolExecutor(10, 10, 60, TimeUnit.SECONDS, new SynchronousQueue<>());
     }
 
     public static void main(String[] args) throws Exception {
